fd9b504c55a5d4a8c1b6646fcc986ca12f5809a5
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2024-01-25 12:57:38 +0100

    .

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello, world')
+const h1 = document.querySelector('h1')
+const p = document.querySelector('p')
+const parrafito = document.querySelector('.parrafito')
+const pid = document.querySelector('#pid')
+const input = document.querySelector('input')
+
+document.getElementById('pid')
+document.getElementsByClassName('parrafito')
+
+
+h1.innerHTML = 'Patito';
+
