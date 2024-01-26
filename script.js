63b08268d7cc2790a7ce3776b47197f8a39a45b9
VisiGoyo <79362138+gvisiedo@users.noreply.github.com>
2024-01-26 20:29:49 +0100

    .

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ document.getElementsByClassName('parrafito')
 
 h1.innerHTML = 'Patito';
 h1.innerText = 'Patito <br> feo';
+//h1.getAttribute('pantalla')
+//console.log(h1.getAttribute('class'));
+//h1.setAttribute('class', 'rojo');
+
+h1.classList.add('rojo');
+h1.classList.remove('verde')
+h1.classList.toggle('verde')
+h1.classList.contains('verde')
+
+input.value = '456'
+const img = document.createElement('img');
+
+img.setAttribute('src', 'https://www.lavanguardia.com/')
+pid.append(img)
+
+
+
